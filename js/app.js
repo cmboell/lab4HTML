75e641e9d666f361957dc3f8003d1362afb8578b
cmboell <cmboell1221@gmail.com>
2020-08-09 20:28:54 -0500

    added some java and css and html

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@
 
 
 console.log('This file is connected and ready to run');
+// new is a keyword  object that has properties, the date.
+var today = new Date();
+// var is a keyword that makes
+// the word today a variable
+
+// allow to take the today variable and output to page
+document.write(today);
