3d2cbf0d071509b3629f01f6d85da55a8556beac
cmboell <cmboell1221@gmail.com>
2020-08-12 13:43:16 -0500

    cleaned up javascript

--- js/appTwo.js
+++ js/appTwo.js
@@ -1,12 +1,11 @@
 'use strict';
 
 console.log('This file is connected and ready to run');
+
 //   new is a keyword      object that has properties, the date.
 var today = new             Date();
 //var is a keyword that makes
 // the word today a variable
-
-
 //allow to take the today variable and output to page.
 var userName = prompt('Please enter your first name');
 
